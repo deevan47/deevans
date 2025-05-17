881af9cd17b342c88ac40ddb5a98c7e4551b5c3e
deevan <deevan@kali.kali>
2025-05-17 13:25:49 +0530

    Initial commit

---
+++ src/redirect.js
@@ -0,0 +1,1 @@
+window.location.replace("https://deevan47.github.io/");
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-base: process.env.NODE_ENV === "production" ? "/deevans/" : "/",
+  base: '/', // Ensure it's set to '/' for local development
   plugins: [react()],
 })
